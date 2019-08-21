43e3d752149fe578bf45f1ce031444a3c2309546
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-08-21 11:07:58 -0500

    genres test

--- test/app.test.js
+++ test/app.test.js
@@ -37,6 +37,13 @@ describe('GET /apps', () => {
       .expect(400, 'Sort must be either by Rating or App');
   });
 
+  it('should be 400 if genres is incorrect', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ genres: 'MISTAKE' })
+      .expect(400, 'Genres must be one of Action, Puzzle, Strategy, Casual, Arcade, Card');
+  });
+
   it('should sort by Rating', () => {
     return supertest(app)
       .get('/apps')
@@ -54,4 +61,24 @@ describe('GET /apps', () => {
         expect(sorted).to.be.true;
       });
   });
+
+  it('should filter by genre', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({genres : 'Arcade' })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        let filterWorking = true;
+        for (let i = 0; i < res.body.length; i++) {
+          if (!(res.body[i].Genres.includes('Arcade'))) {
+            filterWorking = false;
+            i = res.body.length;
+          }
+        }
+        expect(filterWorking).to.be.true;
+      });
+  });
+
 });
